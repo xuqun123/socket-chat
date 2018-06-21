cd63fb4f9faf43baf270a29f608e999227127fbe
Quintin <xuqunlele@gmail.com>
2018-06-21 18:57:35 +1000

    1. change chat server to allow admin choose room to communicate; 2. change client view

--- socket-chat.js
+++ socket-chat.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -7,22 +8,50 @@ app.get('/socket.io', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var adminSocket;
 io.on('connection', function(socket){
-  console.log('an user connected');
-  io.emit('connected', 'an user has been connected');
+  socket.username = 'anonymous_' + socket.id;
+  socket.room = 'room_' + socket.username;
+  socket.admin = false;
 
-  socket.on('add user', function(username) {
-    socket.username = username;
+  console.log('an user connected: ' + socket.id);
+  io.emit('connected', 'an user has been connected: ' + socket.username);
+
+  socket.on('add user', function(data) {
+    socket.username = data.username;
+    socket.room = 'room##' + data.username + '##' + data.email;    
+
+    if (data.username == 'admin' && data.email == 'lele') {
+      socket.admin = true;
+      console.log('admin logged in: ' + socket.username + " (" + socket.room + ")");
+      adminSocket = socket.id;
+      socket.emit('authenticated', io.sockets.adapter.rooms);
+    } else {
+      socket.join(socket.room);
+      console.log('username changed: ' + socket.username + " (" + socket.room + ")");      
+      socket.to(adminSocket).emit('authenticated', io.sockets.adapter.rooms);
+    }
+    console.log('admin socket: ' + adminSocket);
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg + " (" + socket.username + ")");
-    socket.broadcast.emit('chat message', {msg: msg, username: socket.username});
+    console.log('message: ' + msg + " (" + socket.room + ': ' + socket.username + ')');
+    socket.to(socket.room).emit('chat message', {msg: msg, username: socket.username});
+  });
+
+  socket.on('change room', function(room){
+    if (socket.admin){
+      socket.leave(socket.room);
+      socket.room = room;
+      socket.join(socket.room);
+    } else {
+      socket.emit('unauthenticated', 'Only admin can change room!');
+    }
   });
 
   socket.on('disconnect', function(){
     console.log('user disconnected' + " (" + socket.username + ")");
-    io.emit('disconnected', 'an user has been disconnected' + " (" + socket.username + ")");
+    io.emit('disconnected', 'an user has been disconnected: ' + socket.username);
   });  
 });
 
