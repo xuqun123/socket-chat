bc5a8f8e9401b3245257211174672e31c70fc72b
Quintin <xuqunlele@gmail.com>
2018-06-23 16:25:00 +1000

    1. add mysql pack to server; 2. insert each message into DB; 3. tweak view to show DB messages of a specific room

--- socket-chat.js
+++ socket-chat.js
@@ -1,13 +1,28 @@
+//initialize express, server and socket.io
 var app = require('express')();
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
+
+//connect to DB
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'quintin',
+  password: 'xuqun0630',
+  database: 'portfolio'
+});
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('You are now connected to DB...');
+});
 
 app.get('/socket.io', function(req, res){
   // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
+//socket io connection
 var adminSocket;
 io.on('connection', function(socket){
   socket.username = 'anonymous_' + socket.id;
@@ -19,6 +34,7 @@ io.on('connection', function(socket){
 
   socket.on('add user', function(data) {
     socket.username = data.username;
+    socket.email = data.email;
     socket.room = 'room##' + data.username + '##' + data.email;    
 
     if (data.username == 'admin' && data.email == 'lele') {
@@ -39,6 +55,15 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg + " (" + socket.room + ': ' + socket.username + ')');
     socket.to(socket.room).emit('chat message', {msg: msg, username: socket.username});
+    console.log('INSERT INTO messages (body, room) VALUES ("' + msg + '", "' + socket.room + '");');
+    connection.query('INSERT INTO messages (username, email, body, is_admin, room, created_at) VALUES ("' 
+                      + socket.username + '", "' + socket.email + '", "'  
+                      + msg + '", "' + (socket.admin === true ? 1 : 0) + '", "' 
+                      + socket.room + '", "' +  moment().utc().format('YYYY-MM-DD HH:mm:ss') + '");',
+                      function (error, rows) {
+      if (error) throw error;
+      console.log(rows);
+    });    
   });
 
   socket.on('change room', function(room){
