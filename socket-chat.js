ebe2013dabbd909112f3b5336bcd4eccb0a3197e
Quintin <xuqunlele@gmail.com>
2018-06-25 18:24:32 +1000

    1. add 'room left' and 'typing' events

--- socket-chat.js
+++ socket-chat.js
@@ -30,28 +30,28 @@ io.on('connection', function(socket){
   socket.admin = false;
 
   console.log('an user connected: ' + socket.id);
-  io.emit('connected', 'an user has been connected: ' + socket.username);
+  io.emit('connected', socket.username + 'has been connected');
 
   socket.on('add user', function(data) {
     socket.username = data.username;
     socket.email = data.email;
     socket.room = 'room##' + data.username + '##' + data.email;    
 
-    if (data.username == 'admin' && data.email == 'lele') {
+    if (data.username === 'admin' && data.email === 'lele') {
       socket.admin = true;
       console.log('admin logged in: ' + socket.username + " (" + socket.room + ")");
       adminSocket = socket.id;
       socket.emit('authenticated', io.sockets.adapter.rooms);
-      socket.broadcast.emit('admin online', 'admin logged in');
+      socket.broadcast.emit('admin online', 'Quintin is online');
     } else {
       socket.join(socket.room);
       socket.emit('room joined', {username: socket.username, room: socket.room});
 
       console.log('username changed: ' + socket.username + " (" + socket.room + ")");      
       if (adminSocket) {
-        socket.emit('admin online', 'admin logged in');
+        socket.emit('admin online', 'Quintin is online');
         socket.to(adminSocket).emit('authenticated', io.sockets.adapter.rooms);
-        socket.to(adminSocket).emit('room joined', {username: socket.username, room: socket.room, not_admin: true});
+        socket.to(adminSocket).emit('room joined', {username: socket.username, room: socket.room});
       }
     }
     console.log('admin socket: ' + adminSocket);
@@ -74,19 +74,38 @@ io.on('connection', function(socket){
 
   socket.on('change room', function(room){
     if (socket.admin){
+      socket.to(socket.room).emit('room left', {username: socket.username, room: socket.room});
+      socket.emit('room left', {username: socket.username, room: socket.room});
       socket.leave(socket.room);
+      console.log('admin left room: ' + socket.room);
+
       socket.room = room;
+
       socket.join(socket.room);
+      socket.to(socket.room).emit('room joined', {username: socket.username, room: socket.room});
       socket.emit('room joined', {username: socket.username, room: socket.room});
+
+      console.log('admin changed room to: ' + socket.room);
     } else {
       socket.emit('unauthenticated', 'Only admin can change room!');
+      console.log('a non-admin is try to change room: ' + socket.username);
     }
   });
 
   socket.on('disconnect', function(){
+    socket.to(socket.room).emit('room left', {username: socket.username, room: socket.room});
+    socket.to(adminSocket).emit('room left', {username: socket.username, room: socket.room});
     console.log('user disconnected' + " (" + socket.username + ")");
-    io.emit('disconnected', 'an user has been disconnected: ' + socket.username);
+    io.emit('disconnected', socket.username + ' has been disconnected: ');
   });  
+
+  socket.on('typing', function(msg){
+    socket.to(socket.room).emit('typing', msg);    
+  });
+
+  socket.on('typing stopped', function(msg){
+    socket.to(socket.room).emit('typing stopped', msg);    
+  });
 });
 
 http.listen(6001, function(){
