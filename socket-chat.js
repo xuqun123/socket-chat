5fa011eabd254a9f96e71d5a35960d43cd8acd73
Quintin <xuqunlele@gmail.com>
2018-06-29 10:25:32 +1000

    1. add io-file upload pack; 2. tweak node server & client to accept file upload

--- socket-chat.js
+++ socket-chat.js
@@ -1,9 +1,11 @@
 //initialize express, server and socket.io
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 require('dotenv').config();
+const SocketIOFile = require('socket.io-file');
 
 //connect to DB
 var mysql = require('mysql')
@@ -23,6 +25,12 @@ connection.connect(function(err) {
 app.get('/socket.io', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/socket.io-file-client.js', (req, res, next) => {
+  return res.sendFile(__dirname + '/node_modules/socket.io-file-client/socket.io-file-client.js');
+});
+
+app.use('/data', express.static('data'));
 
 //socket io connection
 var adminSocket;
@@ -115,6 +123,37 @@ io.on('connection', function(socket){
   socket.on('typing stopped', function(msg){
     socket.to(socket.room).emit('typing stopped', msg);    
   });
+
+  var uploader = new SocketIOFile(socket, {
+        // uploadDir: {     // multiple directories
+        //  music: 'data/music',
+        //  document: 'data/document'
+        // },
+        uploadDir: 'data',              // simple directory
+        // accepts: ['audio/mpeg', 'audio/mp3'],   // chrome and some of browsers checking mp3 as 'audio/mp3', not 'audio/mpeg'
+        maxFileSize: 4194304,             // 4 MB. default is undefined(no limit)
+        chunkSize: 10240,             // default is 10240(1KB)
+        transmissionDelay: 0,           // delay of each transmission, higher value saves more cpu resources, lower upload speed. default is 0(no delay)
+        overwrite: true               // overwrite file if exists, default is true.
+    });
+    uploader.on('start', (fileInfo) => {
+        console.log('Start uploading');
+        console.log(fileInfo);
+    });
+    uploader.on('stream', (fileInfo) => {
+        console.log(`${fileInfo.wrote} / ${fileInfo.size} byte(s)`);
+    });
+    uploader.on('complete', (fileInfo) => {
+        console.log('Upload Complete.');
+        console.log(fileInfo);
+        socket.broadcast.emit('upload completed', fileInfo);
+    });
+    uploader.on('error', (err) => {
+        console.log('Error!', err);
+    });
+    uploader.on('abort', (fileInfo) => {
+        console.log('Aborted: ', fileInfo);
+    });  
 });
 
 http.listen(6001, function(){
